5a89d6edf8e79cda979bf9ed29a1630b9c1af057
Sejal K. Parmar <sejalkp2@illinois.edu>
2017-09-27 12:04:42 -0500

    fixed checkstyle errors

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -6,21 +6,45 @@
  * 3. Improve the name of the methods and variables
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
+ *
  */
+/**
+ * @author Sejal
+ *
+ */
+public class ReclamationProject {
+    /**
+     *
+     * @param a is a string
+     * @param b is a string
+     * @return r does something
+     */
+    public static String doit(final String a, final String b) {
+        String r, tempA = "", tempB = "";
+        if (a.length() > b.length()) { //if a is longer than b
+            String c = a; // set c to a
+            tempA = b;
+            tempB = c; //switches values of a and b
+            } //end if
+        if (tempA.equals(tempB)) { //if the strings are the same
+            r = "";
+        } else {
+            r = "";
+        }
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    } //end if
+                } //end for
+            } // end for
+        } // end for
+        return r;
+        }
 }
